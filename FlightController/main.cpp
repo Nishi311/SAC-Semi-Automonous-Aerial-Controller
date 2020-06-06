6f3574367aa02c1334c236e09a24eb00af791fa1
Nishi311 <michaelnishida@gmail.com>
2020-06-06 19:13:25 -0400

    Initial push to Master with basic setup

---
+++ FlightController/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * FlightController.cpp
+ *
+ * Created: 6/6/2020 6:54:17 PM
+ * Author : micha
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
